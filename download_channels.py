0cb6f4314f0c992739ffea38f7e41b3d36647b56
Geraldo Guiesi <geraldo.guiesi@gmail.com>
2016-04-27 23:19:43 -0300

    include new columns in tables and include other channels to download

--- download_channels.py
+++ download_channels.py
@@ -1,10 +1,9 @@
 from youtube2feed import YdlDownloader, Feed
 
-lst = [#'https://www.youtube.com/user/Pirulla25',
-       #'https://www.youtube.com/channel/UCsMj2K36ejga547ge5cBscg',
-       #'https://www.youtube.com/channel/UCfQ98EX3oOv6IHBdUNMJq8Q',
-       'https://www.youtube.com/watch?v=os2uOCamqM4',
-       #'http://www.youtube.com/user/chicothepa',
+lst = ['https://www.youtube.com/user/Pirulla25',
+       'https://www.youtube.com/channel/UCsMj2K36ejga547ge5cBscg',
+       'https://www.youtube.com/channel/UCfQ98EX3oOv6IHBdUNMJq8Q',
+       'http://www.youtube.com/user/chicothepa',
        ]
 
 ydl = YdlDownloader(lst)
--- youtube2feed.sql
+++ youtube2feed.sql
@@ -1,17 +1,20 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "episode" (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`title`	TEXT NOT NULL,
-	`pub_date`	TEXT NOT NULL,
-	`url`	TEXT NOT NULL UNIQUE,
-	`channel_id`	INTEGER NOT NULL,
-	`flg_feed`	NUMERIC NOT NULL DEFAULT 0,
-	FOREIGN KEY(`channel_id`) REFERENCES `channel`(`id`) ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS "channel" (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`name`	TEXT NOT NULL UNIQUE,
-	`url`	TEXT NOT NULL UNIQUE,
-	`image_url`	TEXT NOT NULL
-);
+CREATE TABLE "episode" (
+                    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    `title` TEXT NOT NULL,
+                    `pub_date`  TEXT NOT NULL,
+                    `webpage_url` TEXT NOT NULL UNIQUE,
+                    `url`   TEXT NOT NULL UNIQUE,
+                    `channel_id`    INTEGER NOT NULL,
+                    `flg_feed`  NUMERIC NOT NULL DEFAULT 0,
+                    `last_update` TEXT NOT NULL,
+                    FOREIGN KEY(`channel_id`) REFERENCES `channel`(`id`) ON DELETE CASCADE
+                );
+CREATE TABLE "channel" (
+                    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    `name`  TEXT NOT NULL UNIQUE,
+                    `url`   TEXT NOT NULL UNIQUE,
+                    `image_url` TEXT NOT NULL,
+                    `last_update` TEXT NOT NULL
+                );
 COMMIT;
